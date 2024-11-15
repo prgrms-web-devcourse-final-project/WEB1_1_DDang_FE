2b2dd05e2f95cbf46e477fe196172a5362092adf
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-15 19:28:34 +0900

    🔨 Setting: theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import PWABadge from '@/PWABadge'
+import { router } from '@/router'
+import GlobalStyle from '@/styles/globalStyle'
+import { lightTheme, darkTheme } from '@/styles/theme'
+import { useState } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import PWABadge from './PWABadge.tsx'
-import { router } from './router.tsx'
-import GlobalStyle from './styles/globalStyle.ts'
+import { ThemeProvider } from 'styled-components'
 
 function App() {
+  //* 다크모드 확장성 고려
+  const [theme, setTheme] = useState(lightTheme)
+  const toggleTheme = () => setTheme(prev => (prev === lightTheme ? darkTheme : lightTheme))
+
   return (
     <>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-      <PWABadge />
+      <ThemeProvider theme={theme}>
+        <button onClick={toggleTheme} hidden>
+          Toggle Theme
+        </button>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+        <PWABadge />
+      </ThemeProvider>
     </>
   )
 }
