9a6dad12f42cec099fc55173824b7ea576b973c7
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-16 17:46:32 +0900

    🐛 Fix: numberOfRuns 5

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,16 +1,18 @@
-// lighthouserc.cjs
 module.exports = {
   ci: {
     collect: {
       startServerCommand: 'npm run preview',
       url: ['http://localhost:4173'],
-      numberOfRuns: 1,
+      numberOfRuns: 5,
       startServerReadyPattern: 'Local',
     },
     upload: {
       target: 'filesystem',
       outputDir: './lhci_reports',
       reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
     },
   },
 }
