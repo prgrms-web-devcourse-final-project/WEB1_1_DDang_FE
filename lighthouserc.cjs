86ae45c7366a411ecd0a731e570ff567102523bf
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-16 11:27:19 +0900

    ✨ Feat: Github Action lighthouse

---
+++ lighthouserc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      startServerCommand: 'npm run dev',
+      url: ['https://localhost:3000'],
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
