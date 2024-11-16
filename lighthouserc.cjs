47e1c727aba780f697edda00509d768636fbe7c5
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-16 13:51:30 +0900

    🐛 Fix: Vite에 맞춰 lighthouse 관련 수정

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,18 +1,25 @@
+// lighthouserc.cjs
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './build',
-      numberOfRuns: 1,
       startServerCommand: 'npm run dev',
       url: ['https://localhost:3000'],
+      numberOfRuns: 1,
+      startServerReadyPattern: 'ready in',
+      settings: {
+        chromeFlags: ['--ignore-certificate-errors', '--no-sandbox', '--disable-gpu', '--headless'],
+      },
     },
-    // assert: {
-    //   preset: 'lighthouse:recommended',
-    // },
     upload: {
-      target: 'filesystem',
-      outputDir: './lhci_reports',
-      reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'csp-xss': 'off',
+        'unused-javascript': 'off',
+        'uses-http2': 'off',
+      },
     },
   },
 }
