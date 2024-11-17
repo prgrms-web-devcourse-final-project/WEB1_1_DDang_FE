3f5caa60012cb771e18e0805cc74526bae66bbcb
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-18 00:55:22 +0900

    🐛Fix: HomaPage meta description

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <S.HomePage>
       <Helmet>
         <title>DDang | 반려견 산책 서비스</title>
-        <meta name='description' content='반려견과 함께하는 즐거운 산책, DDang과 함께하세요.' />
+        <meta name='description' content='반려견과 함께하는 즐거운 산책, DDang.' />
       </Helmet>
       HomePage
     </S.HomePage>
