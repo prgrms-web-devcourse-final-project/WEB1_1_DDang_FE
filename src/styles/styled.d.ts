9e175eb28dbe5e6c6f1f630cb35a0724a7d452f2
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-20 23:35:51 +0900

    ♻️Refactor: styled.d.ts type 모두 export

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,44 +1,42 @@
-// styled.d.ts
 import 'styled-components'
 
-interface BrandColors {
-  darken: string
-  default: string
-  lighten_1: string
-  lighten_2: string
-  lighten_3: string
-  sub: string
-}
-
-interface GrayscaleColors {
-  font_1: string
-  font_2: string
-  font_3: string
-  font_4: string
-  gc_1: string
-  gc_2: string
-  gc_3: string
-  gc_4: string
-}
-
-interface Colors {
-  brand: BrandColors
-  grayscale: GrayscaleColors
-}
-
-interface Typography {
-  _24: string
-  _20: string
-  _17: string
-  _15: string
-  _14: string
-  _13: string
-  _11: string
-}
-
 declare module 'styled-components' {
-  export interface DefaultTheme {
+  export type DefaultTheme = {
     colors: Colors
     typography: Typography
+  }
+  export type BrandColors = {
+    darken: string
+    default: string
+    lighten_1: string
+    lighten_2: string
+    lighten_3: string
+    sub: string
+  }
+
+  export type GrayscaleColors = {
+    font_1: string
+    font_2: string
+    font_3: string
+    font_4: string
+    gc_1: string
+    gc_2: string
+    gc_3: string
+    gc_4: string
+  }
+
+  export type Colors = {
+    brand: BrandColors
+    grayscale: GrayscaleColors
+  }
+
+  export type Typography = {
+    _24: string
+    _20: string
+    _17: string
+    _15: string
+    _14: string
+    _13: string
+    _11: string
   }
 }
