6d766887c99c7dc09f01662d79614d6dbf6891f2
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-21 00:37:05 +0900

    ✨Feat: ToggleBox type

--- src/components/ToggleBox/index.tsx
+++ src/components/ToggleBox/index.tsx
@@ -1,20 +1,31 @@
 import Toggle from '@components/Toggle'
+import { Typo14, Typo15, Typo17 } from '@components/Typo/Typo'
+import { SETTINGS_INFO } from '@constants/settingsInfo'
 import { SettingsStoreKey } from '@stores/settingsStore'
 import * as S from './styles'
-import { SETTINGS_INFO } from '@constants/settingsInfo'
-import { Typo15, Typo17 } from '@/styles/common.typos'
 
 type ToggleBoxProps = {
+  type: 'sm' | 'md' | 'lg'
   setting: SettingsStoreKey
 }
 
-//todo 부가설명란
-export default function ToggleBox({ setting }: ToggleBoxProps) {
+const Typo = {
+  sm: Typo14,
+  md: Typo15,
+  lg: Typo17,
+}
+
+/**
+ *주의! 단독으로 사용할 경우, Wrapper로 감싸주셔야 border radius가 적용됩니다.
+ */
+export default function ToggleBox({ setting, type }: ToggleBoxProps) {
+  const SelectedTypo = Typo[type]
+
   return (
-    <S.ToggleBox>
+    <S.ToggleBox $type={type}>
       <S.MainArea>
-        <Typo17 weight={700}>{SETTINGS_INFO[setting].title}</Typo17>
-        {SETTINGS_INFO[setting].desc ? <Typo15>{SETTINGS_INFO[setting].desc}</Typo15> : null}
+        <SelectedTypo weight='700'>{SETTINGS_INFO[setting].title}</SelectedTypo>
+        {SETTINGS_INFO[setting].desc && <Typo15>{SETTINGS_INFO[setting].desc}</Typo15>}
       </S.MainArea>
       <Toggle id={setting} setting={setting} />
     </S.ToggleBox>
--- src/components/ToggleBox/styles.ts
+++ src/components/ToggleBox/styles.ts
@@ -1,11 +1,21 @@
 import { styled } from 'styled-components'
 
-export const ToggleBox = styled.div`
+type ToggleBoxProps = {
+  $type: 'sm' | 'md' | 'lg'
+}
+
+const TOGGLE_BOX_PADDING = {
+  sm: '15.5px 16px 15.5px 20px',
+  md: '16.5px 16px 16.5px 20px',
+  lg: '18px 16px 18px 20px',
+}
+export const ToggleBox = styled.div<ToggleBoxProps>`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 18px 16px 18px 20px;
+  padding: ${({ $type }) => TOGGLE_BOX_PADDING[$type]};
   background-color: ${({ theme }) => theme.colors.grayscale.gc_4};
+
   &:first-of-type {
     border-top-left-radius: 16px;
     border-top-right-radius: 16px;
