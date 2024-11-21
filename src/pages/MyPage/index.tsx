60a0dd3cea184e5166e356cf317f22f7cc753218
kimjuyoung99 <jjassb404@naver.com>
2024-11-22 00:40:13 +0900

    🎨 Design : ActionButton 공통컴포넌트 가져오기

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,5 +1,6 @@
 import * as S from './styles'
 import { Helmet } from 'react-helmet-async'
+// import { ActionButton } from '../../components/Button/ActionButton'
 
 export default function MyPage() {
   return (
@@ -8,7 +9,12 @@ export default function MyPage() {
         <title>DDang | 마이페이지</title>
         <meta name='description' content='나의 반려견 정보와 산책 기록을 관리하세요.' />
       </Helmet>
-      MyPage
+      <p>MyPage</p>
+      <S.ButtonArea>
+        <S.CustomActionButton $type='semiRoundedRect' $bgColor='font_1'>
+          로그아웃
+        </S.CustomActionButton>
+      </S.ButtonArea>
     </S.MyPage>
   )
 }
--- src/pages/MyPage/styles.ts
+++ src/pages/MyPage/styles.ts
@@ -1,3 +1,19 @@
 import { styled } from 'styled-components'
+import { ActionButton } from '../../components/Button/ActionButton'
 
-export const MyPage = styled.div``
+export const MyPage = styled.div`
+  display: flex;
+  padding: 20px;
+  height: 100vh;
+  flex-direction: column; //세로 방향 정렬
+`
+export const ButtonArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: auto;
+  align-items: center;
+  width: 100%;
+`
+export const CustomActionButton = styled(ActionButton)`
+  width: 100%;
+`
