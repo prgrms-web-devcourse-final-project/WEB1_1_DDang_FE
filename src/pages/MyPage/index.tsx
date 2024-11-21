63387a759080dcfbad9b8f7353dbf0ffb4f3f536
kimjuyoung99 <jjassb404@naver.com>
2024-11-22 04:31:01 +0900

    🎨 Design : 프로필 section 디자인

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,8 +1,12 @@
 import * as S from './styles'
 import { Helmet } from 'react-helmet-async'
 import { IoSettingsOutline } from 'react-icons/io5'
+import ProfileImage from 'assets/masterprofile.svg'
+import { Typo13, Typo15, Typo24 } from '@components/Typo'
+import { useTheme } from 'styled-components'
 
 export default function MyPage() {
+  const theme = useTheme()
   return (
     <S.MyPage>
       <Helmet>
@@ -17,6 +21,39 @@ export default function MyPage() {
       </S.HeaderContainer>
 
       <S.MainContainer>
+        <S.ProfileSection>
+          <S.ProfileArea>
+            <img src={ProfileImage} />
+          </S.ProfileArea>
+          <S.ProfileText>
+            <Typo24 weight='800'>닉네임</Typo24>
+            <Typo15 weight='400' style={{ color: theme.colors.grayscale.font_2 }}>
+              용산구 남영구 거주
+            </Typo15>
+            <S.TypoWrap>
+              <Typo13 weight='700'>남자</Typo13>
+              <Typo13 weight='700'>할아버지</Typo13>
+            </S.TypoWrap>
+          </S.ProfileText>
+        </S.ProfileSection>
+
+        <S.CountSectioin>
+          <S.CountArea>
+            <S.CountWrapperBig>23회</S.CountWrapperBig>
+            <S.CountWrapperSmall>누적 산책 횟수</S.CountWrapperSmall>
+          </S.CountArea>
+
+          <S.CountArea>
+            <S.CountWrapperBig></S.CountWrapperBig>
+            <S.CountWrapperSmall></S.CountWrapperSmall>
+          </S.CountArea>
+
+          <S.CountArea>
+            <S.CountWrapperBig></S.CountWrapperBig>
+            <S.CountWrapperSmall></S.CountWrapperSmall>
+          </S.CountArea>
+        </S.CountSectioin>
+
         <S.ButtonArea>
           <S.CustomActionButton $type='semiRoundedRect' $bgColor='font_1'>
             로그아웃
--- src/pages/MyPage/styles.ts
+++ src/pages/MyPage/styles.ts
@@ -1,14 +1,25 @@
 import { styled } from 'styled-components'
 import { ActionButton } from '../../components/Button/ActionButton'
 
-export const MyPage = styled.div``
+export const MyPage = styled.div`
+  width: 375px;
+  height: 698px;
+  border: 1px solid blue;
+  overflow: hidden;
+  position: relative;
+  margin: 0 auto;
+  box-sizing: border-box;
+`
 
 export const MainContainer = styled.div`
   display: flex;
   padding: 20px;
-  height: 90vh;
-  flex-direction: column; //세로 방향 정렬
+  height: calc(100% - 56px);
+  flex-direction: column;
+  box-sizing: border-box;
+  overflow-y: auto;
 `
+
 export const ButtonArea = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,9 +27,11 @@ export const ButtonArea = styled.div`
   align-items: center;
   width: 100%;
 `
+
 export const CustomActionButton = styled(ActionButton)`
   width: 100%;
 `
+
 export const HeaderContainer = styled.div`
   width: 100%;
   height: 56px;
@@ -29,15 +42,78 @@ export const HeaderContainer = styled.div`
   text-align: center;
   font-weight: 700;
   line-height: 150%;
-  padding: 15px 150px 15px 150px;
+  padding: 15px 20px;
+  display: flex;
   justify-content: center;
+  align-items: center;
   position: relative;
+  box-sizing: border-box;
 `
+
 export const SettingIcon = styled.div`
   position: absolute;
   right: 20px;
   display: flex;
   align-items: center;
   cursor: pointer;
-  margin-top: -26px; //근데 이게 맞나..
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+export const ProfileSection = styled.div`
+  width: 100%;
+  height: 285px;
+  flex-shrink: 0;
+  background-color: white;
+  border-radius: 16px;
+  box-sizing: border-box;
+`
+
+export const CountSectioin = styled(ProfileSection)`
+  width: 100%;
+  height: 96px;
+  flex-shrink: 0;
+  margin-top: 12px;
+`
+//프로필정보
+export const ProfileArea = styled.div`
+  width: 140px;
+  height: 140px;
+  flex-shrink: 0;
+  margin: 24px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const ProfileText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transform: translateY(-10px);
+  gap: 3.5px;
+`
+
+export const TypoWrap = styled.div`
+  display: flex;
+  gap: 10px;
+`
+//횟수 정보
+export const CountArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`
+
+export const CountWrapperBig = styled.div`
+  font-size: ${({ theme }) => theme.typography._20};
+  font-weight: 800;
+  color: ${({ theme }) => theme.colors.grayscale.font_1};
+`
+
+export const CountWrapperSmall = styled.div`
+  font-size: ${({ theme }) => theme.typography._13};
+  color: ${({ theme }) => theme.colors.grayscale.font_1};
+  font-weight: 500;
 `
