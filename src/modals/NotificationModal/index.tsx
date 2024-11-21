a77ffb5790edef247a74a68f5efc76eeb49ddfa0
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-21 20:46:17 +0900

    🎨Design: NotificationModal

---
+++ src/modals/NotificationModal/index.tsx
@@ -0,0 +1,18 @@
+import PrevButton from '~components/Button/PrevButton'
+import * as S from './styles'
+import { Typo17 } from '~components/Typo'
+import NotificationList from '~components/NotificationList'
+import { useModalStore } from '~stores/modalStore'
+
+export default function NotificationModal() {
+  const { popModal } = useModalStore()
+  return (
+    <S.NotificationModal>
+      <PrevButton onClick={popModal} />
+      <S.Header>
+        <Typo17 weight='700'>알림</Typo17>
+      </S.Header>
+      <NotificationList />
+    </S.NotificationModal>
+  )
+}
