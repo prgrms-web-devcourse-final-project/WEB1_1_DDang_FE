df9283bc4a82234b33c321aec44ee8e55c476fc7
kimjuyoung99 <jjassb404@naver.com>
2024-11-22 02:42:47 +0900

    🎨 mypage header, react-icon (설정아이콘)추가

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,20 +1,28 @@
 import * as S from './styles'
 import { Helmet } from 'react-helmet-async'
-// import { ActionButton } from '../../components/Button/ActionButton'
+import { IoSettingsOutline } from 'react-icons/io5'
 
 export default function MyPage() {
   return (
     <S.MyPage>
       <Helmet>
-        <title>DDang | 마이페이지</title>
+        <title>DDang | 마페</title>
         <meta name='description' content='나의 반려견 정보와 산책 기록을 관리하세요.' />
       </Helmet>
-      <p>MyPage</p>
-      <S.ButtonArea>
-        <S.CustomActionButton $type='semiRoundedRect' $bgColor='font_1'>
-          로그아웃
-        </S.CustomActionButton>
-      </S.ButtonArea>
+      <S.HeaderContainer>
+        마이페이지
+        <S.SettingIcon>
+          <IoSettingsOutline cursor='pointer' size={28} />
+        </S.SettingIcon>
+      </S.HeaderContainer>
+
+      <S.MainContainer>
+        <S.ButtonArea>
+          <S.CustomActionButton $type='semiRoundedRect' $bgColor='font_1'>
+            로그아웃
+          </S.CustomActionButton>
+        </S.ButtonArea>
+      </S.MainContainer>
     </S.MyPage>
   )
 }
--- src/pages/MyPage/styles.ts
+++ src/pages/MyPage/styles.ts
@@ -1,10 +1,12 @@
 import { styled } from 'styled-components'
 import { ActionButton } from '../../components/Button/ActionButton'
 
-export const MyPage = styled.div`
+export const MyPage = styled.div``
+
+export const MainContainer = styled.div`
   display: flex;
   padding: 20px;
-  height: 100vh;
+  height: 90vh;
   flex-direction: column; //세로 방향 정렬
 `
 export const ButtonArea = styled.div`
@@ -16,4 +18,26 @@ export const ButtonArea = styled.div`
 `
 export const CustomActionButton = styled(ActionButton)`
   width: 100%;
+`
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 56px;
+  flex-shrink: 0;
+  border: 1px solid red;
+  color: ${({ theme }) => theme.colors.grayscale.font_1};
+  font-size: ${({ theme }) => theme.typography._17};
+  text-align: center;
+  font-weight: 700;
+  line-height: 150%;
+  padding: 15px 150px 15px 150px;
+  justify-content: center;
+  position: relative;
+`
+export const SettingIcon = styled.div`
+  position: absolute;
+  right: 20px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  margin-top: -26px; //근데 이게 맞나..
 `
