62fb6d2ee50152bc973f84a2545089d62bcbf7b8
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-22 13:35:50 +0900

    ✨Feat: Header Component

--- src/components/Button/PrevButton/styles.ts
+++ src/components/Button/PrevButton/styles.ts
@@ -2,10 +2,6 @@ import { styled } from 'styled-components'
 import { GrPrevious } from 'react-icons/gr'
 
 export const PrevButton = styled(GrPrevious)`
-  position: absolute;
-  left: 20px;
-  top: 14px;
   width: 28px;
   height: 28px;
-  z-index: 5;
 `
--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,2 +1,5 @@
-export const FOOTER_HEIGHT = 56
 export const HEADER_HEIGHT = 56
+export const HEADER_HEIGHT_LG = 80
+export const HEADER_Z_INDEX = 80
+
+export const FOOTER_HEIGHT = 56
