f74dae2c23289c5e86c5b2e173c825bfb695c768
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-22 15:57:42 +0900

    🚀Perf: font display, preconnect

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,8 +2,14 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'npm run preview',
-      url: ['http://localhost:4173'],
-      numberOfRuns: 5,
+      url: [
+        'http://localhost:4173',
+        // 'http://localhost:4173/log',
+        // 'http://localhost:4173/walk',
+        // 'http://localhost:4173/mypage',
+        // 'http://localhost:4173/login',
+      ],
+      numberOfRuns: 1,
       startServerReadyPattern: 'Local',
     },
     upload: {
@@ -11,11 +17,8 @@ module.exports = {
       outputDir: './lhci_reports',
       reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
     },
-    // assert: {
-    //   preset: 'lighthouse:recommended',
-    //   assertions: {
-    //     'csp-xss': ['warn', {}],
-    //   },
-    // },
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
   },
 }
