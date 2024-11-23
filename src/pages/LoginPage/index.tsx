84dcea90c1096ed41ad88751519bf8b202891623
kimjuyoung99 <jjassb404@naver.com>
2024-11-23 18:43:08 +0900

    🎨 Design : 소셜로그인 Sectioin 작업, 각 소셜 로고 sgv파일 import

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,6 +1,5 @@
 import * as S from './styles'
 import { Helmet } from 'react-helmet-async'
-import DogProfileSection from './DogProfileSection'
 
 export default function LoginPage() {
   return (
@@ -15,6 +14,20 @@ export default function LoginPage() {
         <S.BrandText>댕</S.BrandText>과 함께해요!
       </S.TitleSection>{' '}
       <S.Logo>로고</S.Logo>
+      <S.SocialLoginSection>
+        <S.Kakao weight='700'>
+          <S.KakaoIcon />
+          카카오계정 로그인
+        </S.Kakao>
+        <S.Naver weight='700'>
+          <S.NaverIcon />
+          네이버로 로그인
+        </S.Naver>
+        <S.Gogle weight='700'>
+          <S.GoogleIcon />
+          구글로 로그인
+        </S.Gogle>
+      </S.SocialLoginSection>
     </S.LoginPageContainer>
   )
 }
--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -1,13 +1,21 @@
-import { styled } from 'styled-components'
+import { FontWeight, styled } from 'styled-components'
+import Kakao_Icon from '~assets/kakao_icon.svg?react'
+import Naver_Icon from '~assets/naver_icon.svg?react'
+import Google_Icon from '~assets/google_icon.svg?react'
 
 export const LoginPageContainer = styled.div`
-  width: 375px;
-  height: 698px;
+  width: 100%;
+  height: 100dvh;
+  background-color: ${({ theme }) => theme.colors.grayscale.gc_4};
+
   overflow: hidden;
   position: relative;
   margin: 0 auto;
   box-sizing: border-box;
   padding: 0;
+  align-items: center;
+  flex-direction: column;
+  display: flex;
 `
 export const TitleSection = styled.div`
   color: ${({ theme }) => theme.colors.grayscale.font_1};
@@ -15,6 +23,8 @@ export const TitleSection = styled.div`
   font-weight: 700;
   white-space: pre-line; //줄바꿈!
   text-align: center;
+  margin-top: 40%;
+  margin-bottom: 30px;
 `
 export const BrandText = styled.span`
   color: ${({ theme }) => theme.colors.brand.default};
@@ -32,4 +42,84 @@ export const Logo = styled.div`
 
   font-size: ${({ theme }) => theme.typography._24};
   color: ${({ theme }) => theme.colors.grayscale.font_1};
+`
+
+//소셜로그인
+export const SocialLoginSection = styled.div`
+  /* border: 1px solid red; */
+  width: 350px;
+  height: 200px;
+  margin-top: 50px;
+`
+export const Kakao = styled.div<{ weight: FontWeight }>`
+  width: 335px;
+  height: 52px;
+  flex-shrink: 0;
+  border-radius: 12px;
+  background: #ffed16;
+
+  color: ${({ theme }) => theme.colors.grayscale.font_1};
+  font-size: ${({ theme }) => theme.typography._14};
+  font-weight: ${({ weight }) => weight};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin: 10px 6px;
+  cursor: pointer;
+`
+export const KakaoIcon = styled(Kakao_Icon)`
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  left: 24px;
+`
+export const Naver = styled.div<{ weight: FontWeight }>`
+  width: 335px;
+  height: 52px;
+  flex-shrink: 0;
+  border-radius: 12px;
+  background: #03cf5d;
+
+  color: ${({ theme }) => theme.colors.grayscale.gc_4};
+  font-size: ${({ theme }) => theme.typography._14};
+  font-weight: ${({ weight }) => weight};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin: 10px 6px;
+  cursor: pointer;
+`
+export const NaverIcon = styled(Naver_Icon)`
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  left: 24px;
+`
+export const Gogle = styled.div<{ weight: FontWeight }>`
+  width: 335px;
+  height: 52px;
+  flex-shrink: 0;
+  border-radius: 12px;
+  background: #f2f2f2;
+
+  color: ${({ theme }) => theme.colors.grayscale.font_1};
+  font-size: ${({ theme }) => theme.typography._14};
+  font-weight: ${({ weight }) => weight};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin: 10px 6px;
+  cursor: pointer;
+`
+export const GoogleIcon = styled(Google_Icon)`
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  left: 24px;
 `
