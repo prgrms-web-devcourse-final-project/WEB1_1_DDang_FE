3015e667a61fb350f85d1231a22497252932c53b
kimjuyoung99 <jjassb404@naver.com>
2024-11-23 18:55:28 +0900

    🐛 fix : 주석 삭제 (lighthouse 규칙위반)

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -8,7 +8,6 @@ export default function LoginPage() {
         <title>DDang | 로그인</title>
         <meta name='description' content='DDang 서비스 로그인' />
       </Helmet>
-      {/* <DogProfileSection /> */}
       <S.TitleSection>
         건강한 반려 생활{'\n'}
         <S.BrandText>댕</S.BrandText>과 함께해요!
