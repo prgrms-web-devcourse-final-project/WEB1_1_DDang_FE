cb45796a5097197a4f417ebe2ef927adf475e30d
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-23 18:50:03 +0900

    🔧Chore: lighthouse 산책 페이지만 제외

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -5,11 +5,11 @@ module.exports = {
       url: [
         'http://localhost:4173',
         'http://localhost:4173/log',
-        'http://localhost:4173/walk',
+        // 'http://localhost:4173/walk',
         'http://localhost:4173/login',
         'http://localhost:4173/mypage',
       ],
-      numberOfRuns: 5,
+      numberOfRuns: 1,
       startServerReadyPattern: 'Local',
     },
     upload: {
