9b6590160a42324bd1853ecf33be684773436a52
wonil <nwi6959@gmail.com>
2024-11-25 03:06:57 +0900

    ✨  Feat: 반려견 프로필 입력받기 기능 완성

--- src/modals/RegisterDogModal/DogProfileSection/index.tsx
+++ src/modals/RegisterDogModal/DogProfileSection/index.tsx
@@ -1,17 +1,21 @@
-import { useRef, useState } from 'react'
 import * as S from './styles'
+import { useRef, useState } from 'react'
 import { ActionButton } from '~components/Button/ActionButton'
 import AddDogPicture from '~assets/add-dog-picture.svg'
 import TwoLineInput from '~components/Input/TwoLineInput'
 import Header from '~components/Header/index'
 import { Typo24 } from '~components/Typo/index'
-import { DatePicker } from 'ios-style-picker'
-import '/node_modules/ios-style-picker/dist/style.css'
+import { useModalStore } from '~stores/modalStore'
+import DatePickerModal from '~modals/DatePickerModal'
 
 export default function DogProfileSection() {
   const [dogImage, setDogImage] = useState<string | undefined>(undefined)
+  const [dogName, setDogName] = useState('')
+  const [birth, setBirth] = useState('')
+  const [intro, setIntro] = useState('')
 
   const fileInputRef = useRef<HTMLInputElement>(null)
+  const { pushModal } = useModalStore()
 
   const handleClickPrev = () => {}
 
@@ -28,6 +32,10 @@ export default function DogProfileSection() {
 
   const handlePictureAddBtn = () => {
     fileInputRef.current?.click()
+  }
+
+  const handleDatePickerOpen = () => {
+    pushModal(<DatePickerModal date={birth} setDate={setBirth} />)
   }
 
   return (
@@ -49,26 +57,16 @@ export default function DogProfileSection() {
           </S.AddDogPictureBtn>
         </S.AddDogPictureBtnWrapper>
         <S.InputArea>
-          <S.NameInput placeholder='이름 입력' />
-          <S.DatePickerBtn>생년월일 선택</S.DatePickerBtn>
-          <TwoLineInput placeholder='한줄 소개 입력'>한줄 소개 입력</TwoLineInput>
+          <S.NameInput placeholder='이름 입력' value={dogName} onChange={e => setDogName(e.target.value)} />
+          <S.DatePickerBtn onClick={handleDatePickerOpen} hasBirth={!!birth}>
+            {birth || '생년월일 선택'}
+          </S.DatePickerBtn>
+          <TwoLineInput placeholder='한줄 소개 입력' value={intro} onChange={e => setIntro(e.target.value)}>
+            한줄 소개 입력
+          </TwoLineInput>
         </S.InputArea>
         <ActionButton>다음</ActionButton>
       </S.DogProfileSection>
-      <DatePicker
-        fromDate={new Date(2000, 0, 1)}
-        toDate={new Date(new Date().getFullYear(), 11, 31)}
-        infinite
-        initDate={new Date()}
-        onChange={(y, m, d) => {
-          console.log(y, m, d)
-        }}
-        formatters={{
-          year: value => `${value}년`,
-          month: value => `${value}월`,
-          day: value => `${value}일`,
-        }}
-      />
     </>
   )
 }
--- src/modals/RegisterDogModal/DogProfileSection/styles.ts
+++ src/modals/RegisterDogModal/DogProfileSection/styles.ts
@@ -1,78 +1,78 @@
 import { styled } from 'styled-components'
 
 export const DogProfileSection = styled.div`
-    padding : 120px 20px 24px 20px;
-    background-color: ${({theme}) => theme.colors.grayscale.gc_4};
-    height: 100dvh;
-    
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  padding: 120px 20px 24px 20px;
+  background-color: ${({ theme }) => theme.colors.grayscale.gc_4};
+  height: 100dvh;
 
-    @media (max-height : 700px) {
-        padding: 100px 20px 24px 20px;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media (max-height: 700px) {
+    padding: 100px 20px 24px 20px;
+  }
 `
 
 export const PrevBtnWrapper = styled.div`
-    height: 56px;
+  height: 56px;
 
-    @media (max-height: 700px) {
-        margin-bottom: 10px;
-    }
+  @media (max-height: 700px) {
+    margin-bottom: 10px;
+  }
 `
 
 export const TypoWrapper = styled.div`
-    text-align: center;
+  text-align: center;
 `
 
 export const AddDogPictureBtnWrapper = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `
 
 export const AddDogPictureBtn = styled.div`
-    position: relative;
-    overflow: hidden;
-    width: 180px;
-    height: 180px;
-    background-color: ${({theme}) => theme.colors.brand.lighten_2};
-    border-radius: 50%;
+  position: relative;
+  overflow: hidden;
+  width: 180px;
+  height: 180px;
+  background-color: ${({ theme }) => theme.colors.brand.lighten_2};
+  border-radius: 50%;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap : 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 0.5rem;
 
-    font-weight : 700;
-    color: ${({theme}) => theme.colors.brand.darken};
-    cursor: pointer;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.brand.darken};
+  cursor: pointer;
 `
 
 export const HiddenFileInput = styled.input`
-    display: none;
+  display: none;
 `
 
-export const DogImage = styled.img<{hasImage: boolean}>`
-    position: absolute;
-    z-index: ${({hasImage}) => hasImage ? '100' : '-100'};
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    object-fit: cover;
+export const DogImage = styled.img<{ hasImage: boolean }>`
+  position: absolute;
+  z-index: ${({ hasImage }) => (hasImage ? '100' : '-100')};
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover;
 `
 
 export const InputArea = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    @media (min-height: 700px) {
-        margin-bottom: 40px;
-        gap: 0.3rem;
-    }
+  @media (min-height: 700px) {
+    margin-bottom: 40px;
+    gap: 0.3rem;
+  }
 `
 
 export const NameInput = styled.input`
@@ -84,19 +84,20 @@ export const NameInput = styled.input`
   font-size: ${({ theme }) => theme.typography._20};
   font-weight: 700;
   &:focus {
-      box-shadow: ${({ theme }) => `inset 0 0 0 1px ${theme.colors.grayscale.font_1}`};
-    }
+    box-shadow: ${({ theme }) => `inset 0 0 0 1px ${theme.colors.grayscale.font_1}`};
+  }
   &::placeholder {
-    font-weight : 400;
+    font-weight: 400;
   }
 `
 
-export const DatePickerBtn = styled.div`
+export const DatePickerBtn = styled.div<{ hasBirth: boolean }>`
   width: 100%;
   border: none;
   text-align: center;
   padding: 17px 32px;
   font-size: ${({ theme }) => theme.typography._20};
-  color: ${({theme}) => theme.colors.grayscale.font_3};
+  color: ${({ theme, hasBirth }) => (hasBirth ? 'black' : theme.colors.grayscale.font_3)};
+  font-weight: ${({ hasBirth }) => (hasBirth ? '700' : '400')};
   cursor: pointer;
 `
