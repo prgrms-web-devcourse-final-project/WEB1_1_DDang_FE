0f88b6802b46cd847f332b6fa7093a2eb8087d37
wonil <nwi6959@gmail.com>
2024-11-25 13:35:41 +0900

    ✨ Feat: 반려견 프로필 등록 유효성 검사 조건 추가

--- src/utils/validateDogProfile.ts
+++ src/utils/validateDogProfile.ts
@@ -5,10 +5,14 @@ interface DogProfileType {
   intro: string
 }
 
+const regex = /^[가-힣]+$/
+
 export const validateDogProfile = (dogProfile: DogProfileType): string | null => {
   if (!dogProfile.name) return '반려견의 이름을 입력해주세요'
+  if (!regex.test(dogProfile.name) || dogProfile.name.length > 10) return '최대 10자의 한글이름만 사용해 주세요'
   if (!dogProfile.image) return '반려견의 사진을 등록해주세요'
   if (!dogProfile.birth) return '반려견의 생일을 입력해주세요'
   if (!dogProfile.intro) return '한줄 소개를 적어주세요'
+  if (dogProfile.intro.length > 50) '한줄 소개는 최대 50자까지 가능합니다'
   return null
 }
