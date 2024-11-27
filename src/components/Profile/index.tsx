758f566cfae4ae4e269d1097a9dfc3712a868a05
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-28 00:36:26 +0900

    🐛Fix: Profile 클릭 시 clearModal

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import * as S from './styles'
+import { useModalStore } from '~stores/modalStore'
 
 type ProfileProps = {
   $size: number
@@ -8,7 +9,12 @@ type ProfileProps = {
 }
 
 export default function Profile({ $size, $src, userId }: ProfileProps) {
+  const { clearModal } = useModalStore()
   const navigate = useNavigate()
-  const onClick = () => userId && navigate(`/profile/${userId}`)
-  return <S.Profile $size={$size} $src={$src} onClick={onClick} />
+  const onClick = () => {
+    if (!userId) return
+    navigate(`/profile/${userId}`)
+    clearModal()
+  }
+  return <S.Profile $size={$size} $src={$src} $userId={userId} onClick={onClick} />
 }
