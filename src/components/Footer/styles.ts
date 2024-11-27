caa27c7cf19b5fbc65900cd8256ec196f453150d
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-27 14:02:32 +0900

    🎨Design: Footer fixed

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,14 @@
 import { styled } from 'styled-components'
+import { FOOTER_HEIGHT } from '~constants/layout'
 
-export const Footer = styled.nav``
+export const Footer = styled.footer`
+  height: ${FOOTER_HEIGHT}px;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.grayscale.gc_4};
+  box-shadow: inset 0 1px 0 0 ${({ theme }) => theme.colors.grayscale.gc_1};
+`
 
 export const FooterNavList = styled.ul`
   display: flex;
