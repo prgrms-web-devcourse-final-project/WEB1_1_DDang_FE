86956f10c6a5626dcaf94e8990bdb840931a5694
wonil <nwi6959@gmail.com>
2024-11-27 16:03:07 +0900

    🎨 Design: 산책기록 페이지 헤더 추가

--- src/pages/LogPage/index.tsx
+++ src/pages/LogPage/index.tsx
@@ -1,6 +1,8 @@
-import { Typo15 } from '~components/Typo'
+import Header from '~components/Header'
+import { Profile } from '~components/Profile'
 import * as S from './styles'
 import { Helmet } from 'react-helmet-async'
+import GraphIcon from '~/assets/graph.svg'
 
 export default function LogPage() {
   return (
@@ -9,7 +11,11 @@ export default function LogPage() {
         <title>DDang | 산책 기록</title>
         <meta name='description' content='반려견과의 소중한 산책 기록을 확인하세요.' />
       </Helmet>
-      <Typo15>LogPage</Typo15>
+      <Header type={'sm'}>
+        <Profile $size={32} $src='test.svg' />
+        <S.dogName>밤톨이 일기</S.dogName>
+        <S.graphImage src={GraphIcon} alt='' />
+      </Header>
     </S.LogPage>
   )
 }
--- src/pages/LogPage/styles.ts
+++ src/pages/LogPage/styles.ts
@@ -1,3 +1,12 @@
 import { styled } from 'styled-components'
 
 export const LogPage = styled.div``
+
+export const dogName = styled.p`
+  font-weight: bold;
+  margin-left: 10px;
+`
+
+export const graphImage = styled.img`
+  margin-left: auto;
+`
