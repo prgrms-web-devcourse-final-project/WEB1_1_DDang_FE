23737417cb864a2c95dcbe09a6ef6caf2b3973aa
kimjuyoung99 <jjassb404@naver.com>
2024-12-02 12:36:22 +0900

    🔨 Setting : ownerProfile 상태관리 추가 Zustand로 통합

--- src/pages/RegisterPage/Register/index.tsx
+++ src/pages/RegisterPage/Register/index.tsx
@@ -2,20 +2,25 @@ import * as S from './styles'
 import { Helmet } from 'react-helmet-async'
 import AddOwnerAvatar from '~assets/add-dog-picture.svg'
 import GenderSelectButton from '~components/GenderSelectButton'
-import { useState, useEffect } from 'react'
-import TwoLineInput from '~components/Input/TwoLineInput'
+import { useEffect } from 'react'
+import { Input } from '~components/Input'
 import RegisterAvatarModal from '~modals/RegisterAvatarModal'
 import { useModalStore } from '~stores/modalStore'
 import { ActionButton } from '~components/Button/ActionButton'
 import PositionChoiceModal from '~modals/PositionChoiceModal'
 import { useGeolocation } from '~hooks/useGeolocation'
-
+import { useOwnerProfileStore } from '~stores/ownerProfileStore'
+import { validateOwnerProfile } from '~utils/validateOwnerProfile'
+import RegisterDogPage from '~pages/RegisterPage/Dog'
+  
 export default function Register() {
-  const [selectedGender, setSelectedGender] = useState<'male' | 'female' | null>(null)
-  const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null)
-  const [selectedPosition, setSelectedPosition] = useState<string | null>(null)
+  const { ownerProfile, setOwnerProfile } = useOwnerProfileStore()
   const { location, getCurrentLocation } = useGeolocation()
-  const [userLocation, setUserLocation] = useState<string | null>(null)
+  const pushModal = useModalStore(state => state.pushModal)
+
+  const handleNextClick = () => {
+    pushModal(<RegisterDogPage />)
+  }
 
   const handleLocationClick = () => {
     getCurrentLocation()
@@ -23,30 +28,27 @@ export default function Register() {
 
   useEffect(() => {
     if (location.address) {
-      setUserLocation(location.address)
+      setOwnerProfile({ location: location.address })
     }
   }, [location])
 
-  const pushModal = useModalStore(state => state.pushModal)
-
   const handleRoleClick = () => {
-    pushModal(<PositionChoiceModal onSelect={handleSelectPosition} initialValue={selectedPosition} />)
-  }
-
-  const handleSelectPosition = (positionSrc: string) => {
-    setSelectedPosition(positionSrc)
+    pushModal(
+      <PositionChoiceModal onSelect={position => setOwnerProfile({ position })} initialValue={ownerProfile.position} />
+    )
   }
 
   const handleAvatarClick = () => {
-    pushModal(<RegisterAvatarModal onSelectAvatar={handleSelectAvatar} initialSelectedAvatar={selectedAvatar} />)
-  }
-
-  const handleSelectAvatar = (avatarSrc: string) => {
-    setSelectedAvatar(avatarSrc)
+    pushModal(
+      <RegisterAvatarModal
+        onSelectAvatar={avatarSrc => setOwnerProfile({ avatar: avatarSrc })}
+        initialSelectedAvatar={ownerProfile.avatar}
+      />
+    )
   }
 
   const handleGenderSelect = (gender: 'male' | 'female') => {
-    setSelectedGender(gender)
+    setOwnerProfile({ gender })
   }
 
   return (
@@ -59,9 +61,9 @@ export default function Register() {
       <S.TextSection weight='700'>견주님에 대해{'\n'}알려주세요</S.TextSection>
 
       <S.AddOwnerAvatarBtnWrapper>
-        {selectedAvatar ? (
+        {ownerProfile.avatar ? (
           <S.Avatar onClick={handleAvatarClick}>
-            <img src={selectedAvatar} alt='선택된 아바타' />
+            <img src={ownerProfile.avatar} alt='선택된 아바타' />
           </S.Avatar>
         ) : (
           <S.AddOwnerAvatarBtn onClick={handleAvatarClick}>
@@ -73,29 +75,39 @@ export default function Register() {
 
       <S.OwnerProfileSection>
         <S.NickNameWrapper>
-          <TwoLineInput placeholder='닉네임 입력' />
+          <Input
+            placeholder='닉네임 입력'
+            value={ownerProfile.nickName}
+            onChange={e => setOwnerProfile({ nickName: e.target.value })}
+          />
         </S.NickNameWrapper>
-        <S.PositionChoiceBtn onClick={handleRoleClick} $hasSelected={!!selectedPosition}>
-          {selectedPosition || '가족 포지션 선택'}
+        <S.PositionChoiceBtn onClick={handleRoleClick} $hasSelected={!!ownerProfile.position}>
+          {ownerProfile.position || '가족 포지션 선택'}
         </S.PositionChoiceBtn>
-        <S.LocationBtn onClick={handleLocationClick} $hasSelected={!!userLocation}>
-          {userLocation || '내 동네 불러오기'}
+        <S.LocationBtn onClick={handleLocationClick} $hasSelected={!!ownerProfile.location}>
+          {ownerProfile.location || '내 동네 불러오기'}
         </S.LocationBtn>
         <S.GenderSelectBtnWrapper>
           <GenderSelectButton
             gender='male'
-            isActive={selectedGender === 'male'}
+            isActive={ownerProfile.gender === 'male'}
             onClick={() => handleGenderSelect('male')}
           />
           <GenderSelectButton
             gender='female'
-            isActive={selectedGender === 'female'}
+            isActive={ownerProfile.gender === 'female'}
             onClick={() => handleGenderSelect('female')}
           />
         </S.GenderSelectBtnWrapper>
       </S.OwnerProfileSection>
 
-      <ActionButton $fontWeight='700'>다음</ActionButton>
+      <ActionButton
+        $fontWeight='700'
+        $bgColor={validateOwnerProfile(ownerProfile) ? 'gc_1' : 'default'}
+        onClick={handleNextClick}
+      >
+        다음
+      </ActionButton>
     </S.RegisterPage>
   )
 }
