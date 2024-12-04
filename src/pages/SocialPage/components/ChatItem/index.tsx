0a61c61452098e84c21903b16538e19ad19cc643
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-12-04 17:23:19 +0900

    ✨Feat: ChatItem createRoom 적용

--- src/pages/SocialPage/components/ChatItem/index.tsx
+++ src/pages/SocialPage/components/ChatItem/index.tsx
@@ -1,25 +1,27 @@
-import * as S from './styles'
+import { FetchChatRoomListResponse } from '~apis/chatRoom/fetchChatRoomList'
+import { useCreateChatRoom } from '~apis/chatRoom/useCreateChatRoom'
+import Profile from '~components/Profile'
 import { Separator } from '~components/Separator'
 import { Typo11, Typo13, Typo17 } from '~components/Typo'
-import Profile from '~components/Profile'
-import { useModalStore } from '~stores/modalStore'
-import ChatModal from '~modals/ChatModal'
-import { FetchChatRoomListResponse } from '~apis/chatRoom/fetchChatRoomList'
+import * as S from './styles'
+import { FAMILY_ROLE } from '~constants/familyRole'
 
 type ChatItemProps = FetchChatRoomListResponse[number]
 
-export default function ChatItem({ chatRoomId, lastMessage, members, name, unreadMessageCount }: ChatItemProps) {
-  const { pushModal } = useModalStore()
+export default function ChatItem({ lastMessage, members, name, unreadMessageCount }: ChatItemProps) {
+  const { createRoom } = useCreateChatRoom()
+
+  const onClickChatItem = () => createRoom({ opponentMemberId: members[0].memberId })
 
   return (
-    <S.ChatItem onClick={() => pushModal(<ChatModal chatRoomId={chatRoomId} userId={members[0].memberId} />)}>
+    <S.ChatItem onClick={onClickChatItem}>
       <Profile $src={members[0].profileImg} $size={48} userId={members[0].memberId} />
       <S.TypoWrapper>
         <S.UserInfoWrapper>
           <Typo17 $weight='700'>{name}</Typo17>
           <S.DetailWrapper>
             <Typo13 $color='font_2' $weight='500'>
-              {members[0].familyRole}
+              {FAMILY_ROLE[members[0].familyRole]}
             </Typo13>
             <Separator $height={8} />
             <Typo13 $color='font_2' $weight='500'>
