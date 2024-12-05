c173d3df14327aa00d143578bd1b3feaee88f342
kimjuyoung99 <jjassb404@naver.com>
2024-12-06 00:43:14 +0900

    🎨 Design : 코드공유페이지 UI작업

--- src/modals/FamilyDDangModal/ShareCodeModal/index.tsx
+++ src/modals/FamilyDDangModal/ShareCodeModal/index.tsx
@@ -2,9 +2,8 @@ import * as S from './styles.ts'
 import { useModalStore } from '~stores/modalStore.ts'
 import { useTheme } from 'styled-components'
 import { Typo17, Typo24, Typo15 } from '~components/Typo/index.ts'
-import DogImage from '~assets/dog_standup.svg'
 import Header from '~components/Header/index.tsx'
-import { PrevBtn } from '~components/Button/PrevBtn.ts'
+import DogImage from '~assets/dog_standup.svg?react'
 
 export default function ShareCodeModal() {
   const { popModal } = useModalStore()
@@ -12,6 +11,24 @@ export default function ShareCodeModal() {
   return (
     <S.ShareCodeModal>
       <Header type='lg' prevBtn onClickPrev={popModal}></Header>
+      <S.MainContainer>
+        <S.CommentSection>
+          <Typo24 $weight='700'>가족과 함께 해요</Typo24>
+          <Typo17 $weight='400'>
+            가족으로 등록된 회원의 앱에서도 동일한 반려견 프로필을 공유하여 산책을 효율적으로 관리할 수 있어요.
+          </Typo17>
+        </S.CommentSection>
+        <S.DogImageWrapper>
+          <DogImage />
+        </S.DogImageWrapper>
+      </S.MainContainer>
+      <S.CodeShareSection>
+        <S.CodeShareButtonWrapper>
+          <S.CodeShareButton>
+            <Typo15 $weight='700'>초대 코드 복사</Typo15>
+          </S.CodeShareButton>
+        </S.CodeShareButtonWrapper>
+      </S.CodeShareSection>
       코드 공유 공간입니다~!
     </S.ShareCodeModal>
   )
--- src/modals/FamilyDDangModal/ShareCodeModal/styles.ts
+++ src/modals/FamilyDDangModal/ShareCodeModal/styles.ts
@@ -1,7 +1,40 @@
-import * as S from './styles.ts'
 import { styled } from 'styled-components'
-
+import { HEADER_HEIGHT } from '~constants/layout'
 
 export const ShareCodeModal = styled.div`
- 
-`;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  position: relative;
+  background-color: ${({ theme }) => theme.colors.grayscale.gc_4};
+  overflow: hidden;
+  padding: ${HEADER_HEIGHT + 30}px 90px 0 20px;
+
+  @media (max-height: 700px) {
+    padding: 3.3rem 1.25rem 1rem;
+    gap: 0.5rem;
+  }
+`
+
+export const MainContainer = styled.div`
+  margin: 1rem;
+`
+
+export const CommentSection = styled.div`
+  border: 1px solid red;
+`
+
+export const DogImageWrapper = styled.div`
+  white-space: pre-line;
+  padding-bottom: 1rem;
+  padding-left: 10rem;
+  justify-content: left;
+  width: 137.651px;
+  height: 176px;
+`
+
+export const CodeShareSection = styled.div``
+
+export const CodeShareButtonWrapper = styled.div``
+
+export const CodeShareButton = styled.div``
