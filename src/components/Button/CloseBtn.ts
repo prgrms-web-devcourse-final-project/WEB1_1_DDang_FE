cff5501bd8fc524c5b819aa2650e359b47f11e77
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-12-05 13:11:05 +0900

    🎨Design: cursor pointer 적용

--- src/components/Button/CloseBtn.ts
+++ src/components/Button/CloseBtn.ts
@@ -1,4 +1,6 @@
 import { styled } from 'styled-components'
 import Close from '~assets/buttons/close_btn.svg?react'
 
-export const CloseBtn = styled(Close)``
+export const CloseBtn = styled(Close)`
+  cursor: pointer;
+`
