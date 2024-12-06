a27cb0b3de5874afc5d432fda4ed678790bc8042
wonil <nwi6959@gmail.com>
2024-12-06 10:51:21 +0900

    📝 Docs: positionLabelMap 공통 컴포넌트로 분리

--- src/pages/RegisterPage/Register/index.tsx
+++ src/pages/RegisterPage/Register/index.tsx
@@ -15,18 +15,8 @@ import Toast from '~components/Toast'
 import { useToastStore } from '~stores/toastStore'
 import { useSearchParams } from 'react-router-dom'
 import { createRegister } from '~apis/register/createRegister'
+import { positionLabelMap } from '~utils/positionLabelMap'
 import { FamilyRole } from '~types/common'
-
-const positionLabelMap: Record<FamilyRole, string> = {
-  MOTHER: '엄마',
-  FATHER: '아빠',
-  OLDER_BROTHER: '형',
-  ELDER_BROTHER: '오빠',
-  ELDER_SISTER: '언니',
-  OLDER_SISTER: '누나',
-  GRANDFATHER: '할아버지',
-  GRANDMOTHER: '할머니',
-}
 
 export default function Register() {
   const { ownerProfile, setOwnerProfile } = useOwnerProfileStore()
