3bd81d8337ab3813abf0a9272f7aa967a6369f77
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-12-07 23:10:35 +0900

    ✨Feat: requestFriend API

---
+++ src/apis/friend/requestFriend.ts
@@ -0,0 +1,16 @@
+import { axiosInstance } from '~apis/axiosInstance'
+import { APIResponse, CommonAPIResponse } from '~types/api'
+
+export type RequestFriendRequest = {
+  memberId: number
+}
+
+export type RequestFriendResponse = Pick<
+  CommonAPIResponse,
+  'memberId' | 'name' | 'email' | 'provider' | 'gender' | 'address' | 'familyRole' | 'profileImg'
+>
+
+export const requestFriend = async (req: RequestFriendRequest): Promise<APIResponse<RequestFriendResponse>> => {
+  const { data } = await axiosInstance.post<APIResponse<RequestFriendResponse>>(`/friend`, req)
+  return data
+}
