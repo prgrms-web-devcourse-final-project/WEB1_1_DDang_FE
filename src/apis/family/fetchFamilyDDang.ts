e0c7c6899196a19a57dfc93f463b49eb16dc421a
kimjuyoung99 <jjassb404@naver.com>
2024-12-09 14:19:24 +0900

    🐛 Remove : 주석삭제

--- src/apis/family/fetchFamilyDDang.ts
+++ src/apis/family/fetchFamilyDDang.ts
@@ -2,19 +2,7 @@ import { AxiosError } from 'axios'
 import { APIResponse, CommonAPIResponse, ErrorResponse, Member } from '~types/api'
 import { axiosInstance } from '~apis/axiosInstance'
 import { DayOfWeek } from '~types/common'
-// export type FetchFamilyDDangResponse = Pick<
-//   CommonAPIResponse,
-//   'familyId' | 'members' | 'dogs' | 'totalWalkCount' | 'totalDistanceInKilometers' | 'totalCalorie'
-// > & {
-//   members: (Pick<Member, 'memberId' | 'email' | 'name' | 'gender' | 'familyRole' | 'profileImg'> & {
-//     walkScheduleInfoList: {
-//       walkScheduleId: number
-//       dayOfWeek: DayOfWeek
-//       walkTime: string
-//     }[]
-//     totalWalkCount: number // 추가된 속성
-//   })[]
-// }
+
 export type FetchFamilyDDangResponse = Pick<
   CommonAPIResponse,
   'familyId' | 'dogs' | 'totalWalkCount' | 'totalDistanceInKilometers' | 'totalCalorie'
